ad24450c7f05f9c0533eaab680a8425740c38ebd
Juanjgw <juanjgw@gmail.com>
2024-04-07 03:13:07 -0300

    primeros calculos selectTComienzo

--- src/componentes/formCantidadTramos.jsx
+++ src/componentes/formCantidadTramos.jsx
@@ -6,11 +6,38 @@ const FormCantidadTramos = ({ params, open, handleClose, handleSubmit, ...props 
   const [optionSelected, setOptionSelected] = useState("1");
 
   const calcularTramos = () => {
-
     // Reemplazar por calculo cantidad de tramos
     // params cuenta con toda la informacion necesaria
-    console.log("Parametros recibidos para calculo: ", params)
+    // Calcular la cantidad de columnas necesarias
+    const cantidad_vidrios = Math.floor(params.largoTramo / 1250);
+    // Calcular la longitud de cada tramo
+    const longitud_tramo = Math.floor((params.largoTramo - ((cantidad_vidrios - 1) * 21)) / cantidad_vidrios);
+    const cantidad_columnas = cantidad_vidrios + 1;
+    var comienzaEn, terminaEn;
+
+    // Comienzo
+    console.log("tcomienzo:", params.selectTComienzo)
+    console.log("Parametros recibidos para calculo antes switch: ", params); 
+    switch (params.selectTComienzo) {
+      case "1":
+        comienzaEn = 38; // descuento por tipo C
+        break;
+      case "2":
+        comienzaEn = 43; // descuento por tipo L
+        break;
+      default:
+        // Manejar el caso en que params.tcomienzo no sea ni 1 ni 2
+        console.error("Valor no válido para params.tcomienzo:", params.selectTComienzo);
+        break;
+    }
+    console.log(`comienzaEn=${comienzaEn}`);
     
+    // Imprimir los resultados
+    console.log("Cantidad de columnas necesarias:", cantidad_columnas);
+    console.log("Longitud de los vidrios:", longitud_tramo);
+   
+  
+    console.log("Parametros recibidos para calculo: ", params);  
     switch (optionSelected) {
       case "2":
         params.cantidadTramos = 2;
